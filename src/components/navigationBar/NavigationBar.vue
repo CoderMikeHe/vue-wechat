9e98b119310d25657eb13fc7c535d4954164f2c2
CoderMikeHe <491273090@qq.com>
2019-02-25 00:36:51 +0800

    增加导航栏，修改Bug

---
+++ src/components/navigationBar/NavigationBar.vue
@@ -0,0 +1,21 @@
+// 全局导航条
+
+<template>
+  <header id="header" class="mui-bar mui-bar-nav">
+    <h1 class="mui-title">导航栏</h1>
+    <button class="mui-action-back mui-btn mui-btn-blue mui-btn-link mui-btn-nav mui-pull-left">
+      <span class="mui-icon mui-icon-left-nav"></span>首页
+    </button>
+    <button class="mui-btn mui-btn-blue mui-btn-link mui-pull-right">编辑</button>
+    <button class="mui-btn mui-btn-blue mui-btn-link mui-pull-right">考虑</button>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
