a63f1403dd8a2d129f8f77af4adbd0336a9ae555
CoderMikeHe <491273090@qq.com>
2019-02-25 23:26:07 +0800

    修改TabBar

--- src/components/tabBar/TabBar.vue
+++ src/components/tabBar/TabBar.vue
@@ -1,25 +1,23 @@
 <template>
   <div>
     <nav class="mh-tab-bar">
-      <router-link class="mh-tab-item mui-active" to="/">
-        <img class="mh-tab-icon" src="../../assets/images/tabBar/tabbar_mainframe_25x23.png" alt="">
+      <router-link class="mh-tab-item" to="/" exact>
+        <img class="mh-tab-icon" src="../../assets/images/tabBar/tabbar_mainframe_25x23.png" alt>
         <span class="mh-tab-label">微信</span>
       </router-link>
       <router-link class="mh-tab-item" to="/contacts">
         <!-- <span class="mh-tab-icon mui-icon-email">
           <span class="mui-badge">9</span>
-        </span> -->
-        <img class="mh-tab-icon" src="../../assets/images/tabBar/tabbar_mainframe_25x23.png" alt="">
+        </span>-->
+        <img class="mh-tab-icon" src="../../assets/images/tabBar/tabbar_contacts_27x23.png" alt>
         <span class="mh-tab-label">通讯录</span>
       </router-link>
       <router-link class="mh-tab-item" to="/discover">
-        <!-- <span class="mui-icon mui-icon-contact"></span> -->
-        <img class="mh-tab-icon" src="../../assets/images/tabBar/tabbar_mainframe_25x23.png" alt="">
+        <img class="mh-tab-icon" src="../../assets/images/tabBar/tabbar_discover_23x23.png" alt>
         <span class="mh-tab-label">发现</span>
       </router-link>
       <router-link class="mh-tab-item" to="/profile">
-        <!-- <span class="mui-icon mui-icon-gear"></span> -->
-        <img class="mh-tab-icon" src="../../assets/images/tabBar/tabbar_mainframe_25x23.png" alt="">
+        <img class="mh-tab-icon" src="../../assets/images/tabBar/tabbar_me_23x23.png" alt>
         <span class="mh-tab-label">我</span>
       </router-link>
     </nav>
@@ -31,11 +29,21 @@ import "weui";
 // import mui from "../../assets/vendor/mui/js/mui.min.js";
 export default {
   data() {
-    return {};
+    return {
+      selectedMainFrame: 1,
+      selectedContacts: 0,
+      selectedDiscover: 0,
+      selectedProfile: 0
+    };
   },
   methods: {
     itemClick(e) {
       console.log(e);
+    }
+  },
+  watch: {
+    $route(to, from) {
+      console.log(to, from);
     }
   }
 };
@@ -48,44 +56,37 @@ export default {
   display: -ms-flexbox;
   display: -webkit-flex;
   display: flex;
-  // flex: auto; // 1 1 auto
-  flex-grow: 1;
   position: fixed;
   z-index: 10;
   right: 0;
   left: 0;
   bottom: 0;
-  height: 49px;
   width: 100%;
-  background-color: antiquewhite;
-  align-items: center;
+  background-color: white;
 }
 
 // 该类名，解决 tabbar 点击无法切换的问题
-// .mui-bar-tab .mh-tab-item {
-//   color: #0f0;
-// }
-
 .mh-tab-bar .mh-tab-item {
+  flex: auto;
   overflow: hidden;
   height: 49px;
-  // width: 25%;
   text-align: center;
-  // vertical-align: middle;
+  vertical-align: middle;
   white-space: nowrap;
   text-overflow: ellipsis;
   color: #929292;
-  display: block;
 }
-
+.mh-tab-bar .mh-tab-item.router-link-active {
+  color: #0f0;
+}
 .mh-tab-bar .mh-tab-item .mh-tab-icon {
+  position: relative;
   top: 3px;
   width: 24px;
   height: 24px;
   padding-top: 0;
   padding-bottom: 0;
 }
-
 .mh-tab-bar .mh-tab-item .mh-tab-icon ~ .mh-tab-label {
   font-size: 11px;
   display: block;
