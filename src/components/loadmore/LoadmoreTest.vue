dead2623d0eda12b8a643886bcd16dd5739a1069
CoderMikeHe <491273090@qq.com>
2020-04-04 09:54:01 +0800

    test

--- src/components/loadmore/LoadmoreTest.vue
+++ src/components/loadmore/LoadmoreTest.vue
@@ -2,14 +2,13 @@
   <div class="loadmore__wrapper">
     <div class="xxoo">toubu</div>
     <div class="ooxx">
-      <Loadmore
-        ref="loadmore"
-        :top-method="refreshBoqList"
-        @translate-change="translateChange"
-        :bottom-method="loadMoreData"
-        :bottom-all-loaded="allLoaded"
-      >
-        <h1 v-for="(item, index) in items" :key="index">
+      <Loadmore ref="loadmore"
+                :top-method="refreshBoqList"
+                @translate-change="translateChange"
+                :bottom-method="loadMoreData"
+                :bottom-all-loaded="allLoaded">
+        <h1 v-for="(item, index) in items"
+            :key="index">
           {{ "这是第" + index + "行" }}
         </h1>
       </Loadmore>
@@ -19,7 +18,7 @@
 </template>
 
 <script>
-import Loadmore from "./Loadmore";
+import Loadmore from './Loadmore'
 export default {
   components: {
     Loadmore
@@ -29,37 +28,37 @@ export default {
       allLoaded: false,
       items: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
       page: 0
-    };
+    }
   },
   methods: {
     refreshBoqList() {
       setTimeout(() => {
-        this.page = 1;
-        let items = [];
+        this.page = 1
+        let items = []
         for (let index = 0; index < 10; index++) {
-          let o = this.page * 10 + index;
-          items.push(o);
+          let o = this.page * 10 + index
+          items.push(o)
         }
-        this.items = items;
-        this.$refs.loadmore.onTopLoaded();
-      }, 2000);
+        this.items = items
+        this.$refs.loadmore.onTopLoaded()
+      }, 2000)
     },
     translateChange(val) {
       // console.log("偏移...", val);
     },
 
     loadMoreData() {
-      let page = this.page + 1;
+      let page = this.page + 1
       setTimeout(() => {
         for (let index = 0; index < 10; index++) {
-          let o = page * 10 + index;
-          this.items.push(o);
+          let o = page * 10 + index
+          this.items.push(o)
         }
-        this.$refs.loadmore.onBottomLoaded();
-      }, 4000);
+        this.$refs.loadmore.onBottomLoaded()
+      }, 4000)
     }
   }
-};
+}
 </script>
 
 <style lang="scss">
@@ -83,8 +82,8 @@ export default {
 }
 
 h1 {
-  height: 44px;
-  line-height: 44px;
+  height: 80px;
+  line-height: 80px;
   background-color: white;
 }
 </style>
